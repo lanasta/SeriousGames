dab6c1aadac2caaac91aeb43d435b3f511f60178
judychung <judy.chung.95@gmail.com>
2016-02-28 15:19:00 -0500

    update

--- SquirrelPacification/Preloader.js
+++ SquirrelPacification/Preloader.js
@@ -1,1 +1,24 @@
-preloader
+SquirrealPacification.Preloader = function(game) {
+	this.preloadBar = null;
+	this.titleText = null;
+	this.ready = false;
+};
+
+SquirrelPacification.Preloader.prototype = {
+
+	preload: function () {
+		this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
+		this.preloadBar.anchor.setTo(0.5, 0.5);
+		this.load.setPreloadSprite(this.preloadBar);
+		this.titleText = this.add.image(this.world.centerX, this.world.centerY-220, 'titleimage');
+		this.titleText.anchor.setTo(0.5, 0.5);
+	},
+
+	create: function () {
+		this.preloadBar.cropEnabled = false; //force show the whole thing
+	},
+
+	update: function () {
+	   	this.ready = true;
+	}
+};
