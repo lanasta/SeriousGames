d3f9e33467843b96309436d9cff30ab582b2f961
judychung <judy.chung.95@gmail.com>
2016-04-13 00:17:47 -0400

    update

--- SquirrelPacification/StartMenu.js
+++ SquirrelPacification/StartMenu.js
@@ -1,19 +1,20 @@
-SquirrelPacification.StartMenu = function(game) {
-    this.TitleBG; // background
+var StartMenu {
+    this.startBG; // background
     this.startPrompt;
-}
 
-BunnyDefender.StartMenu.prototype = {
-
+	preload: function() {
+		game.load.image('titlescreen', 'assets/TitleBG.png');
+	}
 	create: function () { // actually something that's built in phaser
 		startBG = this.add.image(0, 0, 'titlescreen');
 		startBG.inputEnabled = true; // allows accepting mouse clip and touches
 		startBG.events.onInputDown.addOnce(this.startGame, this); // goes to startGame function and passes game
 
-		startPrompt = this.add.bitmapText(this.world.centerX-155, this.world.centerY+180, 'eightbitwonder', 'Touch to Start!', 24);
+		startPrompt = this.add.text(game.world.centerX, game.world.centerY, 'Touch to Start!', {fontSize: '30', fill: 'yellow'});
+		startPrompt.anchor.x = Math.round(startPrompt.width * 0.5) / startPrompt.width;
 	},
 
 	startGame: function (pointer) { // created by him and once click happens
-		this.state.start('Game'); // 'Game' is final state to be created
+		this.state.start('game'); // 'Game' is final state to be created
 	}
 };
